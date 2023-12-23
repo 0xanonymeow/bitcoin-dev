fbe64e22532df9343c2c7601a43ecf0bb9f91185
0xanonymeow <anonymeow@incatni.to>
2023-12-23 08:07:22 +0700

    fix: wallet name

--- create-wallet.sh
+++ create-wallet.sh
@@ -10,25 +10,24 @@ CONTAINER_NAME=bitcoin-core
 AUTH_ARGS="-chain=${CHAIN} -rpcuser=${RPC_USER} -rpcpassword=${RPC_PASSWORD} -rpcport=${RPC_PORT}"
 
 # command to create a new wallet
-CREATE_WALLET_CMD="bitcoin-cli ${AUTH_ARGS} createwallet default"
-CREATE_WALLET_ADDRESS="bitcoin-cli ${AUTH_ARGS} -rpcwallet=default getnewaddress"
+CREATE_WALLET_CMD="bitcoin-cli ${AUTH_ARGS} createwallet ${RPC_USER}"
+CREATE_WALLET_ADDRESS="bitcoin-cli ${AUTH_ARGS} -rpcwallet=${RPC_USER} getnewaddress -addresstype legacy"
 
 # execute the commands inside the Docker container and save the output to a file
-WALLET_INFO=$(docker exec -it ${CONTAINER_NAME} bash -c "${CREATE_WALLET_CMD}")
+WALLET_INFO=$(docker exec ${CONTAINER_NAME} bash -c "${CREATE_WALLET_CMD}")
 
 # check if there are any errors in the output
-if [[ "$WALLET_INFO" == *"Error"* ]]; then
+if [[ "$WALLET_INFO" == *"Error"* && "$WALLET_INFO" != *"already exists"* ]]; then
     echo "error: failed to create wallet."
 else
     # execute the command to get a new address from the created wallet
-    WALLET_ADDRESS=$(docker exec -it ${CONTAINER_NAME} bash -c "${CREATE_WALLET_ADDRESS}" 2>&1)
+    WALLET_ADDRESS=$(docker exec ${CONTAINER_NAME} bash -c "${CREATE_WALLET_ADDRESS}" 2>&1)
 
     # check if there are any errors in the wallet address output
     if [[ "$WALLET_ADDRESS" == *"Error"* ]]; then
         echo "error: failed to get wallet address."
     else
         WALLET_NAME=default
-        
         echo "" >> .env
 
         for key in "WALLET_NAME" "WALLET_ADDRESS"; do
@@ -44,7 +43,5 @@ else
         done
     fi
 fi
-
-
 
 echo "updated .env file with new variables."
